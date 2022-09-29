d935ede6f6e802f390268c489f6b77fde2327abb
mashirnizamikpi <mashir.nizami@kpipartners.com>
2022-09-29 20:48:59 +0530

    Commit for Functional Interface

--- src/Assignment3/src/Assignment3/Punjab.java
+++ src/Assignment3/src/Assignment3/Punjab.java
@@ -1,5 +1,17 @@
 package Assignment3;
 
+interface State{
+	void region();
+}
+
+class Haryana implements State{
+
+@Override
+public void region() {
+	// TODO Auto-generated method stub
+	
+}
+
 public class Punjab {
 	
 	
@@ -7,10 +19,12 @@ public class Punjab {
     {
   
         // lambda expression - Second way to call Functional Interface
-        new Thread(() -> {
-            System.out.println("New thread created");
-        }).start();
+      State haryana = () ->System.out.println("Capital is Chandigarh");
+      haryana.region();
     }
+}
+
+
 }
 
 
